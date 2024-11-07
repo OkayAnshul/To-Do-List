7e6f6f40f5fce0823a41c72b213f4f2297028c7d
Anshul Kumar <anshulisokay@gmail.com>
2024-11-07 19:41:21 +0530

    AddorEdit Screen Added

---
+++ app/src/main/java/com/example/todolist/screen/Add_EditScreen.kt
@@ -0,0 +1,11 @@
+package com.example.todolist.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.todolist.Navigation.Screen
+
+@Composable
+fun AddEditScreen(nav:NavHostController= rememberNavController()){
+
+}
